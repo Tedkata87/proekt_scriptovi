7e0d68d043484944a4480fe927a28421d06f05cf
Tedkata87 <tedkata87g@gmail.com>
2026-06-14 21:52:19 +0300

    login js

---
+++ frontend/js/login.js
@@ -0,0 +1,71 @@
+const loginForm =
+    document.getElementById(
+        "login-form"
+    );
+
+loginForm.addEventListener(
+    "submit",
+    async (e) => {
+
+        e.preventDefault();
+
+        const email =
+            document.getElementById(
+                "email"
+            ).value;
+
+        const password =
+            document.getElementById(
+                "password"
+            ).value;
+
+        const response =
+            await fetch(
+                "http://127.0.0.1:5000/sign-in",
+                {
+                    method: "POST",
+
+                    headers: {
+                        "Content-Type":
+                            "application/json"
+                    },
+
+                    body: JSON.stringify({
+                        email,
+                        password
+                    })
+                }
+            );
+
+        const data =
+            await response.json();
+
+        const message =
+            document.getElementById(
+                "message"
+            );
+
+        if (response.ok) {
+
+            localStorage.setItem(
+                "token",
+                data.token
+            );
+
+            message.innerText =
+                "Login successful";
+
+            setTimeout(() => {
+
+                window.location.href =
+                    "bike-finder.html";
+
+            }, 1000);
+
+        } else {
+
+            message.innerText =
+                data.error;
+        }
+    }
+);
